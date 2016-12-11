a9fafda027bb5ff881b31f27e2c90a3c0876fd38
Aaron Madsen <amadsen@codifiant.com>
2016-12-11 00:00:27 -0700

    So very close to working with GreenRadius U2F server code.

--- resources/u2f/injectable-u2f-support.js
+++ resources/u2f/injectable-u2f-support.js
@@ -1,68 +1,109 @@
 "use strict";
 
 var remote = require('electron').remote,
-  u2fc = require('u2f-client');
+    u2fc = require('u2f-client');
 
 function captureSubmit (event) {
-  var form = event.target;
-  event.preventDefault();
+    var form = event.target;
+    event.preventDefault();
 
-  // prepare form data (just getting inputs because we know we can do that)
-  var data = [].slice.call(form.querySelectorAll('[name]'))
-    .filter( (el) => {
-      return !!el;
-    }).map( (cur) => {
-      return cur.name + '=' + encodeURIComponent(cur.value);
-    });
+    // prepare form data (just getting inputs because we know we can do that)
+    var data = [].slice.call(form.querySelectorAll('[name]'))
+        .filter( (el) => {
+            return !!el;
+        }).map( (cur) => {
+            return cur.name + '=' + encodeURIComponent(cur.value);
+        });
 
-  var submitReq = new XMLHttpRequest();
-  submitReq.onreadystatechange = function () {
-    if(submitReq.readyState === XMLHttpRequest.DONE && submitReq.status === 200) {
-      var raw = submitReq.responseText,
-        registrations = JSON.parse(/registrations\s?=\s?(.+);/m.exec(raw)[1]),
-        request = JSON.parse(/request\s?=\s?(.+);/m.exec(raw)[1]),
-        user = /user\s?=\s?(.+);/m.exec(raw)[1],
-        maxQueueId = /maxQueueId\s?=\s?(.+);/m.exec(raw)[1],
-        hasTimedOut = false;
-
-      console.log(submitReq.responseText);
-      console.log(registrations);
-      console.log(request);
-      console.log(user);
-      console.log(maxQueueId);
-
-      var divForm = document.getElementById('mainBody');
-      var divMessageBody = document.getElementById('messageBody');
-      divForm.style.display = "none";
-      divMessageBody.style.padding = "20px";
-      divMessageBody.innerHTML = "Touch your U2F token";
-
-      u2f.sign(request, function(data) {
-        console.log('U2F signing has returned.');
-        console.log(data);
-        if ( !hasTimedOut && !(data.errorCode > 0) ) {
-          form.getElementById('doAuthenticate').value = data;
-          form.getElementById('request').value = request;
-          form.getElementById('registrations').value = registrations;
-          form.getElementById('user').value = user;
-          form.getElementById('maxId').value = maxQueueId;
-          form.submit();
+    postUrlEncoded({
+        url: form.getAttribute('action'),
+        data: data
+    }, function(err, submitReq){
+        if(err){
+            return console.error(err);
         }
-      });
-      setTimeout(function(){
-        hasTimedOut = true;
-        console.log('U2F signing has timed out.');
-      },1000);
-    }
-  };
 
-  submitReq.open(
-    'POST',
-    form.getAttribute('action') || (''+window.location.href).replace(window.location.search, ''),
-    true
-  );
-  submitReq.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
-  submitReq.send( data.join('&') );
+        var raw = submitReq.responseText,
+            registrations = JSON.parse(/registrations\s?=\s?(.+);/m.exec(raw)[1]),
+            request = JSON.parse(/request\s?=\s?(.+);/m.exec(raw)[1]),
+            user = /user\s?=\s?(.+);/m.exec(raw)[1],
+            maxQueueId = /maxQueueId\s?=\s?(.+);/m.exec(raw)[1],
+            hasTimedOut = false,
+            timer;
+
+        console.log(submitReq.responseText);
+        console.log(registrations);
+        console.log(request);
+        console.log(user);
+        console.log(maxQueueId);
+
+        var divForm = document.getElementById('mainBody');
+        var divMessageBody = document.getElementById('messageBody');
+        divForm.style.display = "none";
+        divMessageBody.style.padding = "20px";
+        divMessageBody.innerHTML = "Touch your U2F token";
+
+        timer = setTimeout(function(){
+            hasTimedOut = true;
+            console.log('U2F signing has timed out.');
+            divMessageBody.innerHTML = "U2F authentication took too long.";
+        }, 15000);
+
+        u2f.sign(request, function(data) {
+            clearTimeout(timer);
+            console.log('U2F signing has returned.');
+            console.log(data);
+
+            if ( !hasTimedOut && data && !(data.errorCode > 0) ) {
+                console.log('POST U2F signing data.');
+                return postUrlEncoded({
+                    data: [
+                    'doAuthenticate=' + encodeURIComponent(JSON.stringify(data)),
+                    'request=' + encodeURIComponent(JSON.stringify(request)),
+                    'registrations=' + encodeURIComponent(JSON.stringify(registrations)),
+                    'user=' + encodeURIComponent(user),
+                    'maxId=' + encodeURIComponent(maxQueueId)
+                    ]
+                }, function (err, req){
+                    var raw = req.responseText,
+                        auth = (/auth\s*=\s*'(.+)';/g.exec(raw) || [])[1] || '',
+                        error = (/var\s+error\s*=\s*'(.*)';/.exec(raw) || [])[1] || '';
+                    console.log(req);
+                    if(err || error) {
+                        divMessageBody.innerHTML = "<p>U2F Authentication Failed:</p>\n"+
+                            (err || '') + ' ' + error;
+                        return;
+                    }
+                    divMessageBody.innerHTML = "U2F Authentication Successful";
+                });
+            }
+            data = data || {};
+            divMessageBody.innerHTML = "<p>U2F authentication error.</p>\n"+
+                data.errorCode +': '+ data.errorMessage;
+        });
+    });
+}
+
+function postUrlEncoded(opt, done){
+    var submitReq = new XMLHttpRequest();
+    submitReq.onreadystatechange = function(){
+        if(submitReq.readyState === XMLHttpRequest.DONE){
+            if(submitReq.status !== 200) {
+                return done(submitReq.status, submitReq);
+            }
+            return done(null, submitReq);
+        }
+    };
+    submitReq.open(
+      opt.method || 'POST',
+      opt.url || (''+window.location.href).replace(window.location.search, ''),
+      true
+    );
+    submitReq.setRequestHeader(
+        "Content-Type",
+        opt.contentType || 'application/x-www-form-urlencoded'
+    );
+    submitReq.send( opt.data.join('&') );
 }
 
 process.once('loaded', () => {
@@ -80,7 +121,7 @@ process.once('loaded', () => {
     var form = document.getElementById('form');
     form.addEventListener('submit', captureSubmit);
 
-    window.addEventListener('load', function(){
+    setTimeout( function(){
       console.log('Checking for startAuthenticate button');
 
       var proceedBtn = document.querySelector('#submitButton');
@@ -89,6 +130,6 @@ process.once('loaded', () => {
           proceedBtn.click();
         },100);
       }
-    });
+  },100);
   });
-})
+});
